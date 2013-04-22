ad0998e569dbe663e6af82f93aec14b784f35d54
hotlib <jackiechanaaa@gmail.com>
2013-04-22 15:54:35 +0200

    validation methods

--- src/main/java/org/gameorganizer/ui/Player.java
+++ src/main/java/org/gameorganizer/ui/Player.java
@@ -3,9 +3,11 @@ package org.gameorganizer.ui;
 import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 import javax.inject.Named;
-
-
 
 @Named
 @SessionScoped
@@ -15,36 +17,61 @@ public class Player implements Serializable {
 	private String name;
 	private String password;
 	private String email;
-	
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getRequest() {
 		return request;
 	}
+
 	public void setRequest(String request) {
 		this.request = request;
 	}
-	
+
+	public void checkEmail(FacesContext context, UIComponent component,
+			Object value) throws ValidatorException {
+
+		if (!username.equals(value))
+			throw new ValidatorException(new FacesMessage("you are not joe"));
+
+	}
+
+	public void checkNickname(FacesContext context, UIComponent component,
+			Object value) throws ValidatorException {
+
+		if (!username.equals(value))
+			throw new ValidatorException(new FacesMessage("you are not joe2"));
+
+	}
+
 }
